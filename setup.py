08cf5b04db17af362c5e6aa5ac0d7f6c40f0f3c2
Michael Bugert <git@mbugert.de>
2021-03-23 08:59:39 +0100

    Bump project version to 0.2.8

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with PRODUCTION_REQUIREMENTS.open() as f:
 
 setup(
     name="connectbox-prometheus",
-    version="0.2.7",
+    version="0.2.8",
     author="Michael Bugert",
     author_email="git@mbugert.de",
     description='Prometheus exporter for Compal CH7465LG cable modems, commonly sold as "Connect Box"',
